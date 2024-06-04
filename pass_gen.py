ba3c93d709cdb10aac14e5752581605b1bafabf5
Sanders003 <122292067+Sanders003@users.noreply.github.com>
2024-06-04 23:03:20 +0530

    Add files via upload

---
+++ pass_gen.py
@@ -0,0 +1,43 @@
+import string as st
+import random as ra
+
+# Taking input of password length
+length = int(input("Enter password length: "))
+
+print('''Choose character set for password from these : 
+		1. Digits
+		2. Letters
+		3. Special characters
+		4. Exit''')
+
+characterList = ""
+
+# Getting character set for password
+while(True):
+	choice = int(input("Pick a number: "))
+	
+	if(choice == 1):
+		# Adding letters to possible characters
+		characterList += st.ascii_letters
+	elif(choice == 2):
+		# Adding digits to possible characters
+		characterList += st.digits
+	elif(choice == 3):
+		# Adding special characters to possible characters
+		characterList += st.punctuation
+	elif(choice == 4):
+		break
+	else:
+		print("Please pick a valid option!")
+
+password = []
+
+for i in range(length):
+    # Picking a random character from our character list
+	randomchar = ra.choice(characterList)
+	
+    # appending a random character to password
+	password.append(randomchar)
+
+# printing password as a string
+print("The random password is " + "".join(password))
